1c806270140b1cc83d36f6105e65002ed026ae65
Johnylab <joaobatistacomunic@hotmail.com>
2021-08-31 22:33:51 -0300

    redux store removed

--- src/app/constants.js
+++
@@ -1,4 +0,0 @@
-export const status = {
-    IDLE: 'IDLE',
-    LOADING: 'LOADING',
-};
--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import searchReducer from './slices/search';
-
-export const store = configureStore({
-  reducer: {
-    search: searchReducer,
-  },
-});
